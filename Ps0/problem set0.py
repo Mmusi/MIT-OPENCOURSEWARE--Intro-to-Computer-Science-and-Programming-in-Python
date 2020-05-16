b37c56ca899b9994be950ece3727b4d31817a382
Thato Mmusi <mmusi2010@gmail.com>
2020-05-16 19:36:24 +0200

    Add files via upload

---
+++ Ps0/problem set0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 13:56:54 2020
+
+@author: Thato Seeletso Mmusi
+"""
+
+import math
+
+x=int(input('Please enter number x:'))
+y=int(input('Please enter number y:'))
+z=x**y
+print('x**y', z)
+print('log(x)',int(math.log2(x)))
